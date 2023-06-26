48e9b7ef2683ec7ba0e034c7cbe9cc59899c5c86
Being-RAYtastic <risheet.ray@gmail.com>
2023-06-26 15:24:21 +0530

    only checks if the its the whole word in itself

--- src/events/messageCreate/greetings.js
+++ src/events/messageCreate/greetings.js
@@ -14,17 +14,36 @@ const greetings_wordList = [
     "hiya",
     "hey there",
     "hola",
+    "hey",
 ]
+// const word = "hello";
+// const regex = new RegExp(`/\b${word}\b/`);
 
+// /\bword\b/.test
 const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 module.exports = (client, message) => {
-    if (message.author.bot) return
+    try {
+        if (message.author.bot) return
 
-    if (greetings_wordList.some(word => ((message.content).toLowerCase()).includes(word))) {
-        const randomGreetingPhrase = Math.floor(Math.random() * greetings_wordList.length)
-        message.channel.send(`${capitalizeFirstLetter(greetings_wordList[randomGreetingPhrase])}!`)
+        const greetingMessageFromUser = greetings_wordList.some(word =>
+            (
+                (
+                    (message.content).toLowerCase()
+                ).split(' ')
+            ).includes(word)
+        )
+
+
+        if (greetingMessageFromUser) {
+            const randomGreetingPhrase = Math.floor(Math.random() * greetings_wordList.length)
+            message.channel.send(`${capitalizeFirstLetter(greetings_wordList[randomGreetingPhrase])}!`)
+        }
+    } catch (error) {
+        console.log(`Some Error occurred: ${error}`)
     }
 }
+
+
