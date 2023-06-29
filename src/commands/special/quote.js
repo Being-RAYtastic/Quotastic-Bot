96e5b8887e76c7719187edb8bdbff34c0c9eea29
Being-RAYtastic <risheet.ray@gmail.com>
2023-06-29 12:03:21 +0530

    fixed crashing

--- src/commands/special/quote.js
+++ src/commands/special/quote.js
@@ -41,45 +41,50 @@ module.exports = {
 
 
     callback: (client, interaction) => {
-        const category = interaction.options.getString('category')
-        // console.log(category)
-        request.get({
+        try {
+            const category = interaction.options.getString('category')
+            // console.log(category)
+            request.get({
 
-            url: category !== null ? `https://api.api-ninjas.com/v1/quotes?category=${category}` : `https://api.api-ninjas.com/v1/quotes?`,
-            headers: {
-                'X-Api-Key': process.env.APININJA_APIKEY
-            },
-        }, function (error, response, body) {
-            if (error) return console.error('Request failed:', error);
-            else if (response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
-            else {
-                const parsedBody = JSON.parse(body)
-                const quote = parsedBody[0].quote
-                const author = parsedBody[0].author
-                const categoryType = parsedBody[0].category
-                // console.log(parsedBody)
+                url: category !== null ? `https://api.api-ninjas.com/v1/quotes?category=${category}` : `https://api.api-ninjas.com/v1/quotes?`,
+                headers: {
+                    'X-Api-Key': process.env.APININJA_APIKEY
+                },
+            }, function (error, response, body) {
+                if (error) return console.error('Request failed:', error);
+                else if (response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+                else {
+                    const parsedBody = JSON.parse(body)
+                    const quote = parsedBody[0].quote
+                    const author = parsedBody[0].author
+                    const categoryType = parsedBody[0].category
+                    // console.log(parsedBody)
 
-                // Will reply to user “”
-                const embed = new EmbedBuilder()
-                    .setTitle(`“${quote}”`)
-                    // .setDescription(`~ ${author}`)
-                    .addFields(
-                        {
-                            name: `- ${author}`,
-                            value: ' ',
-                            inline: true,
-                        },
-                        {
-                            name:" ",
-                            value: `Category: ${capitalizeFirstLetter(categoryType)}`,
-                            inline: true,
-                        }
-                    )
-                    .setColor('DarkBlue')   // * You can add custom color by writing '0xhexcod'  #HEXCODES
+                    // Will reply to user “”
+                    const embed = new EmbedBuilder()
+                        .setTitle(`“${quote}”`)
+                        // .setDescription(`~ ${author}`)
+                        .addFields(
+                            {
+                                name: `- ${author}`,
+                                value: ' ',
+                                inline: true,
+                            },
+                            {
+                                name: " ",
+                                value: `Category: ${capitalizeFirstLetter(categoryType)}`,
+                                inline: true,
+                            }
+                        )
+                        .setColor('DarkBlue')   // * You can add custom color by writing '0xhexcod'  #HEXCODES
 
-                interaction.reply({ embeds: [embed] })
-            }
-        });
+                    interaction.reply({ embeds: [embed] })
+                }
+            })
+        } catch (e) {
+            console.log(`Some Error occurred in quotes.js: ${e}`)
+        }
+
 
     }
 }
